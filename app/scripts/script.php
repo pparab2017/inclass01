f1e838392eedd21d8e241a3322d093d94a1d72f7
Pushparaj Parab <pparab1@uncc.edu>
2017-11-15 11:08:47 -0500

    cronjob

--- app/scripts/script.php
+++ app/scripts/script.php
@@ -13,50 +13,50 @@ require_once '../../vendor/autoload.php';
 require_once '../generated-conf/config.php';
 //$date = new DateTime();
 //
-//$msg = new Messages();
-//$msg->setContent("test123");
-//$msg->setTime($date);
-//$msg->save();
+$msg = new Messages();
+$msg->setContent("test123");
+$msg->setTime($date);
+$msg->save();
+
+
+
+
+
+//// API access key from Google API's Console
+//define( 'API_ACCESS_KEY', 'AIzaSyDBrPwDcNTwIHgl0PFAKLHXMcnCj9_k-s8' );
+//$registrationIds = array( 'ee3oY2eq-Jk:APA91bGgsdB4OplI8UZRUsgLHRMHWhb_P30LSgoVcqvon5jvPvE1deTE_0AzJGSqmomOQBQrc_Rb6U9QNcyhVYon2kJ8H3ktX96Sk9OvsHwje07z37zAzU2_bJuj5Kyo4RPWilfe_FTL' );
+//// prep the bundle
+//$msg = array
+//(
+//    'message' 	=> 'This is a new message',
+//    'title'		=> 'Health App',
+//    'subtitle'	=> 'This is a subtitle. subtitle',
+//    'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
+//    'vibrate'	=> 1,
+//    'sound'		=> 1,
+//    'largeIcon'	=> 'large_icon',
+//    'smallIcon'	=> 'small_icon'
+//);
+//$fields = array
+//(
+//    'registration_ids' 	=> $registrationIds,
+//    'data'			=> $msg,
 //
+//);
 //
-
-
-
-// API access key from Google API's Console
-define( 'API_ACCESS_KEY', 'AIzaSyDBrPwDcNTwIHgl0PFAKLHXMcnCj9_k-s8' );
-$registrationIds = array( 'ee3oY2eq-Jk:APA91bGgsdB4OplI8UZRUsgLHRMHWhb_P30LSgoVcqvon5jvPvE1deTE_0AzJGSqmomOQBQrc_Rb6U9QNcyhVYon2kJ8H3ktX96Sk9OvsHwje07z37zAzU2_bJuj5Kyo4RPWilfe_FTL' );
-// prep the bundle
-$msg = array
-(
-    'message' 	=> 'This is a new message',
-    'title'		=> 'Health App',
-    'subtitle'	=> 'This is a subtitle. subtitle',
-    'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
-    'vibrate'	=> 1,
-    'sound'		=> 1,
-    'largeIcon'	=> 'large_icon',
-    'smallIcon'	=> 'small_icon'
-);
-$fields = array
-(
-    'registration_ids' 	=> $registrationIds,
-    'data'			=> $msg,
-
-);
-
-$headers = array
-(
-    'Authorization: key=' . API_ACCESS_KEY,
-    'Content-Type: application/json'
-);
-
-$ch = curl_init();
-curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
-curl_setopt( $ch,CURLOPT_POST, true );
-curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
-curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
-curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
-$result = curl_exec($ch );
-curl_close( $ch );
-echo $result;
+//$headers = array
+//(
+//    'Authorization: key=' . API_ACCESS_KEY,
+//    'Content-Type: application/json'
+//);
+//
+//$ch = curl_init();
+//curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+//curl_setopt( $ch,CURLOPT_POST, true );
+//curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+//curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+//curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+//curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+//$result = curl_exec($ch );
+//curl_close( $ch );
+//echo $result;
