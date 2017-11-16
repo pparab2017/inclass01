07a6c412e02fd28fcc29f8081f9fb34caf21af9b
Pushparaj Parab <pparab1@uncc.edu>
2017-11-15 20:52:45 -0500

    no message

--- public/index.php
+++ public/index.php
@@ -5,7 +5,11 @@
  * Date: 3/2/16
  * Time: 12:28 AM
  */
-
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use Respect\Validation\Validator as v;
