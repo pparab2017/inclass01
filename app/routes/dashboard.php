e2c00810cec8accca10c3ae99d6492a1f7ce80cd
Pushparaj Parab <pparab1@uncc.edu>
2017-12-14 14:58:47 -0500

    no message

--- app/routes/dashboard.php
+++ app/routes/dashboard.php
@@ -52,7 +52,7 @@ $app->get('/project/coordinator[/[dashboard]]', function ($request, $response, $
     $paramValue = $request-> getQueryParam('error',null);
     if($paramValue!=null)
         $errorString = Utils::getErrorString($paramValue);
-    return $this->view->render($response, 'public.project.admin.dashboard.twig.html', [
+    return $this->view->render($response, 'public.project.coordinator.dashboard.twig.html', [
         "error" => $errorString
     ]);
 })->setName('project.coordinator.dashboard')
